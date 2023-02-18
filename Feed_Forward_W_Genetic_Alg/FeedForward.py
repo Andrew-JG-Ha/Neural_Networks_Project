b43e4800720375d7dfd62e2f8ebfa5c46e96253e
Andrew Ha <andrewha123123@gmail.com>
2023-02-17 23:49:16 -0800

    Added a comment in FeedForward

--- Feed_Forward_W_Genetic_Alg/FeedForward.py
+++ Feed_Forward_W_Genetic_Alg/FeedForward.py
@@ -2,3 +2,8 @@
 This file will be used for the feed forward algorithm for a forward-forward neural network
 
 """
+
+def feedForward():
+    """
+    Function that perfroms the feed forward algorithm
+    """
