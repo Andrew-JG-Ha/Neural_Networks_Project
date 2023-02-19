c8150f4b87cbd2eeaa3d4c75f6349f3057fc6ccc
Andrew Ha <andrewha123123@gmail.com>
2023-02-19 12:02:35 -0800

    Added the prototype of FeedBackward

--- Feed_Forward_W_Genetic_Alg/FeedBackward.py
+++ Feed_Forward_W_Genetic_Alg/FeedBackward.py
@@ -2,3 +2,7 @@
 This file will hold the code dealing with feeding backwards in the neural network
 
 """
+def feedBackwards():
+    """
+    The method used to perform the feeding backwards in a neural network
+    """
